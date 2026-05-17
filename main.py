7ac94d66d5f60c2e64cb40c1b264d08ee05da899
smayatskiy <stasm226@gmail.com>
2026-05-17 13:34:55 +0500

    fix: correct currency router import and update API prefix in main application

--- main.py
+++ main.py
@@ -3,10 +3,13 @@
 """
 
 from fastapi import FastAPI
-from app.api.endpoints.v1.currency import router as currenct_router
+
+from app.api.endpoints.v1.currency import router as currency_router
+from app.api.endpoints.v1.users import router as users_router
 
 app = FastAPI()
-app.include_router(currenct_router, prefix="/v1", tags=["currency"])
+app.include_router(currency_router, prefix="/api/v1", tags=["currency"])
+app.include_router(users_router, prefix="/api/v1", tags=["users"])
 
 if __name__ == "__main__":
     import uvicorn
