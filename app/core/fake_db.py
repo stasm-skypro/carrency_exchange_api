48a7f5492a05ae6979fc11c54df4c872307a3089
smayatskiy <stasm226@gmail.com>
2026-05-20 15:53:18 +0500

    feat: add fake authentication database

---
+++ app/core/fake_db.py
@@ -0,0 +1,10 @@
+"""
+Фейковая база данных для аутентификации.
+"""
+
+FAKE_USERS: dict[str, dict[str, str]] = {
+    "user1": {
+        "username": "user1",
+        "password": "1234",
+    },
+}
