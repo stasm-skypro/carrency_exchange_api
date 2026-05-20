c1b248d316046359cbf3df835c420abdbc48d839
smayatskiy <stasm226@gmail.com>
2026-05-20 19:14:00 +0500

    feat: add test for password mismatch in UserRegister

--- tests/test_models.py
+++ tests/test_models.py
@@ -22,3 +22,20 @@ def test_user_register_success():
     assert user.username == "valid_username"
     assert user.password == "strong_pass_123"
     assert user.password_confirm == "strong_pass_123"
+
+
+def test_password_mismatch():
+    """
+    Проверяет, что схема UserRegister выбрасывает ошибку при несоответствии паролей.
+    """
+
+    data = {
+        "username": "valid_username",
+        "password": "strong_pass_123",
+        "password_confirm": "different_pass_456",
+    }
+
+    with pytest.raises(ValidationError) as excinfo:
+        UserRegister(**data)
+
+    assert "password_confirm does not match password" in str(excinfo.value)
