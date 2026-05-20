a9a79fc97ba103c336a0c2f65bf6479f1f7857ae
smayatskiy <stasm226@gmail.com>
2026-05-20 18:34:47 +0500

    feat: add tests for UserRegister validation

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,3 +3,22 @@
 """
 
 import pytest
+from pydantic import ValidationError
+from app.api.schemas.user import UserRegister
+
+
+def test_user_register_success():
+    """
+    Проверяет, что валидная схема UserRegister проходит без ошибок.
+    """
+
+    data = {
+        "username": "valid_username",
+        "password": "strong_pass_123",
+        "password_confirm": "strong_pass_123",
+    }
+
+    user = UserRegister(**data)
+    assert user.username == "valid_username"
+    assert user.password == "strong_pass_123"
+    assert user.password_confirm == "strong_pass_123"
